ab821e2bfad7092e466cf1b8b0a59137c394dc78
ted <ted@DESKTOP-CS5LEG8>
2018-03-14 11:58:16 -1200

    Test Gethub before start

---
+++ Juke_Box/Juke_Box.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Juke_Box
+{
+    partial class Juke_Box
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Juke_Box
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 450);
+            this.Name = "Juke_Box";
+            this.Text = "Juke_Box";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
