56d202a204585ae4d3b1c8e5c329eb36e92d7853
ted <ted@DESKTOP-CS5LEG8>
2018-03-22 20:19:37 -1200

    Interface_2

--- Juke_Box/Set_up.Designer.cs
+++ Juke_Box/Set_up.Designer.cs
@@ -28,24 +28,235 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lst_Read_Directory = new System.Windows.Forms.ListBox();
+            this.btn_Import_Tracks = new System.Windows.Forms.Button();
+            this.btn_Clear_Tracks = new System.Windows.Forms.Button();
+            this.btn_Coyp_Track = new System.Windows.Forms.Button();
+            this.btn_Move_Track = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Title = new System.Windows.Forms.TextBox();
+            this.lst_Templet = new System.Windows.Forms.ListBox();
+            this.btn_Previous_Title = new System.Windows.Forms.Button();
+            this.btn_Add_Title = new System.Windows.Forms.Button();
+            this.btn_Delete_Title = new System.Windows.Forms.Button();
+            this.btn_Next_Title = new System.Windows.Forms.Button();
+            this.btn_Cancle = new System.Windows.Forms.Button();
+            this.btn_OK = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_Clear_Tracks);
+            this.groupBox1.Controls.Add(this.btn_Import_Tracks);
+            this.groupBox1.Controls.Add(this.lst_Read_Directory);
+            this.groupBox1.Location = new System.Drawing.Point(12, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(364, 520);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Import Tracks";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_Next_Title);
+            this.groupBox2.Controls.Add(this.btn_Delete_Title);
+            this.groupBox2.Controls.Add(this.btn_Add_Title);
+            this.groupBox2.Controls.Add(this.btn_Previous_Title);
+            this.groupBox2.Controls.Add(this.lst_Templet);
+            this.groupBox2.Controls.Add(this.txt_Title);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(598, 26);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(318, 520);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Present Genre";
+            // 
+            // lst_Read_Directory
+            // 
+            this.lst_Read_Directory.FormattingEnabled = true;
+            this.lst_Read_Directory.ItemHeight = 16;
+            this.lst_Read_Directory.Location = new System.Drawing.Point(6, 21);
+            this.lst_Read_Directory.Name = "lst_Read_Directory";
+            this.lst_Read_Directory.Size = new System.Drawing.Size(352, 372);
+            this.lst_Read_Directory.TabIndex = 4;
+            // 
+            // btn_Import_Tracks
+            // 
+            this.btn_Import_Tracks.Location = new System.Drawing.Point(6, 410);
+            this.btn_Import_Tracks.Name = "btn_Import_Tracks";
+            this.btn_Import_Tracks.Size = new System.Drawing.Size(352, 39);
+            this.btn_Import_Tracks.TabIndex = 5;
+            this.btn_Import_Tracks.Text = "Import Tracks From Directory";
+            this.btn_Import_Tracks.UseVisualStyleBackColor = true;
+            // 
+            // btn_Clear_Tracks
+            // 
+            this.btn_Clear_Tracks.Location = new System.Drawing.Point(6, 464);
+            this.btn_Clear_Tracks.Name = "btn_Clear_Tracks";
+            this.btn_Clear_Tracks.Size = new System.Drawing.Size(352, 39);
+            this.btn_Clear_Tracks.TabIndex = 6;
+            this.btn_Clear_Tracks.Text = "Clear Imported Tracks";
+            this.btn_Clear_Tracks.UseVisualStyleBackColor = true;
+            // 
+            // btn_Coyp_Track
+            // 
+            this.btn_Coyp_Track.Location = new System.Drawing.Point(382, 130);
+            this.btn_Coyp_Track.Name = "btn_Coyp_Track";
+            this.btn_Coyp_Track.Size = new System.Drawing.Size(210, 39);
+            this.btn_Coyp_Track.TabIndex = 7;
+            this.btn_Coyp_Track.Text = "Coyp Track >>";
+            this.btn_Coyp_Track.UseVisualStyleBackColor = true;
+            // 
+            // btn_Move_Track
+            // 
+            this.btn_Move_Track.Location = new System.Drawing.Point(382, 192);
+            this.btn_Move_Track.Name = "btn_Move_Track";
+            this.btn_Move_Track.Size = new System.Drawing.Size(210, 39);
+            this.btn_Move_Track.TabIndex = 8;
+            this.btn_Move_Track.Text = "Move Track >>";
+            this.btn_Move_Track.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(382, 359);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(210, 60);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Delete Track From Genre";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Genre Title";
+            // 
+            // txt_Title
+            // 
+            this.txt_Title.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txt_Title.Location = new System.Drawing.Point(36, 41);
+            this.txt_Title.Multiline = true;
+            this.txt_Title.Name = "txt_Title";
+            this.txt_Title.ReadOnly = true;
+            this.txt_Title.Size = new System.Drawing.Size(249, 32);
+            this.txt_Title.TabIndex = 3;
+            // 
+            // lst_Templet
+            // 
+            this.lst_Templet.FormattingEnabled = true;
+            this.lst_Templet.ItemHeight = 16;
+            this.lst_Templet.Location = new System.Drawing.Point(36, 79);
+            this.lst_Templet.Name = "lst_Templet";
+            this.lst_Templet.Size = new System.Drawing.Size(249, 308);
+            this.lst_Templet.TabIndex = 4;
+            // 
+            // btn_Previous_Title
+            // 
+            this.btn_Previous_Title.Location = new System.Drawing.Point(11, 423);
+            this.btn_Previous_Title.Name = "btn_Previous_Title";
+            this.btn_Previous_Title.Size = new System.Drawing.Size(96, 68);
+            this.btn_Previous_Title.TabIndex = 10;
+            this.btn_Previous_Title.Text = "<< Previous";
+            this.btn_Previous_Title.UseVisualStyleBackColor = true;
+            // 
+            // btn_Add_Title
+            // 
+            this.btn_Add_Title.Location = new System.Drawing.Point(117, 423);
+            this.btn_Add_Title.Name = "btn_Add_Title";
+            this.btn_Add_Title.Size = new System.Drawing.Size(86, 26);
+            this.btn_Add_Title.TabIndex = 12;
+            this.btn_Add_Title.Text = "Add";
+            this.btn_Add_Title.UseVisualStyleBackColor = true;
+            // 
+            // btn_Delete_Title
+            // 
+            this.btn_Delete_Title.Location = new System.Drawing.Point(117, 465);
+            this.btn_Delete_Title.Name = "btn_Delete_Title";
+            this.btn_Delete_Title.Size = new System.Drawing.Size(86, 26);
+            this.btn_Delete_Title.TabIndex = 13;
+            this.btn_Delete_Title.Text = "Delete";
+            this.btn_Delete_Title.UseVisualStyleBackColor = true;
+            // 
+            // btn_Next_Title
+            // 
+            this.btn_Next_Title.Location = new System.Drawing.Point(214, 423);
+            this.btn_Next_Title.Name = "btn_Next_Title";
+            this.btn_Next_Title.Size = new System.Drawing.Size(96, 68);
+            this.btn_Next_Title.TabIndex = 14;
+            this.btn_Next_Title.Text = "Next >>";
+            this.btn_Next_Title.UseVisualStyleBackColor = true;
+            // 
+            // btn_Cancle
+            // 
+            this.btn_Cancle.Location = new System.Drawing.Point(812, 573);
+            this.btn_Cancle.Name = "btn_Cancle";
+            this.btn_Cancle.Size = new System.Drawing.Size(104, 30);
+            this.btn_Cancle.TabIndex = 11;
+            this.btn_Cancle.Text = "Cancle";
+            this.btn_Cancle.UseVisualStyleBackColor = true;
+            // 
+            // btn_OK
+            // 
+            this.btn_OK.Location = new System.Drawing.Point(697, 573);
+            this.btn_OK.Name = "btn_OK";
+            this.btn_OK.Size = new System.Drawing.Size(104, 30);
+            this.btn_OK.TabIndex = 12;
+            this.btn_OK.Text = "OK";
+            this.btn_OK.UseVisualStyleBackColor = true;
             // 
             // Set_up
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(928, 669);
+            this.ClientSize = new System.Drawing.Size(928, 615);
+            this.Controls.Add(this.btn_OK);
+            this.Controls.Add(this.btn_Cancle);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btn_Move_Track);
+            this.Controls.Add(this.btn_Coyp_Track);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Set_up";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Set_up";
+            this.Text = "Setup";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox lst_Read_Directory;
+        private System.Windows.Forms.Button btn_Clear_Tracks;
+        private System.Windows.Forms.Button btn_Import_Tracks;
+        private System.Windows.Forms.Button btn_Coyp_Track;
+        private System.Windows.Forms.Button btn_Move_Track;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Title;
+        private System.Windows.Forms.ListBox lst_Templet;
+        private System.Windows.Forms.Button btn_Next_Title;
+        private System.Windows.Forms.Button btn_Delete_Title;
+        private System.Windows.Forms.Button btn_Add_Title;
+        private System.Windows.Forms.Button btn_Previous_Title;
+        private System.Windows.Forms.Button btn_Cancle;
+        private System.Windows.Forms.Button btn_OK;
     }
 }
